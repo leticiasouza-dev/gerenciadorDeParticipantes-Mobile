444829de09750812a77975167258fb97e51748b6
leticiaSouzaDev <lec667121@gmail.com>
2024-12-05 16:03:23 -0300

    commit inicial

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>oiiiiiiiiii</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +12,17 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    color: 'black',
+    backgroundColor: 'red',
+
+  }
+})
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'purple',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
