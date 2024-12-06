fa4e69c450063bbd01ac2415e04ab7f0492edc02
leticiaSouzaDev <lec667121@gmail.com>
2024-12-06 14:21:01 -0300

    feat: adicionando e estilizando o form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -12,15 +12,18 @@ export function Home(){
             <Text style={styles.eventName}>Nome do evento</Text>
             <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
 
-            <TextInput 
-                style={styles.input}
-                placeholder="Nome do participante"
-                placeholderTextColor='#6b6b6b'
-            />
+            <View style={styles.form}>
+                <TextInput 
+                    style={styles.input}
+                    placeholder="Nome do participante"
+                    placeholderTextColor='#6b6b6b'
+                />
 
-            <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-                <Text style={styles.buttonText}>+</Text>
-            </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+                    <Text style={styles.buttonText}>+</Text>
+                </TouchableOpacity>
+            </View>
+            
         </View>
     )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -17,13 +17,22 @@ export const styles = StyleSheet.create({
       color: '#6b6b6b',
       fontSize: 16
     },
+    form:{
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginTop: 36,
+      marginBottom: 42
+    },
     input:{
+     flex: 1,
      backgroundColor: '#1f1e25',
      height: 56,
      borderRadius:5,
      color: '#fff',
      padding: 16,
      fontSize: 16,
+     marginRight: 12
     },
     button: {
       width: 56,
@@ -31,7 +40,7 @@ export const styles = StyleSheet.create({
       borderRadius: 5,
       backgroundColor: '#31cf67',
       alignItems: 'center',
-      justifyContent: 'center',
+      justifyContent: 'center'
     },
     buttonText: {
       color: '#fff',
